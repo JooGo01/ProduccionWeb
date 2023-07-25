8c1b8ad74b0be3d1fdcb2189cd360df83c015e82
JooGo01 <joseakd136@gmail.com>
2023-07-24 23:16:25 -0300

    cambiamos tienda, proceso de hacer los combobox dinamicos, en la tienda, vamos a tener que hacer un index y un show para agregar al carrito, vemos como seguimos, agregamos la funcion al controlador del stock para tomar el dato por jquery al cambiar el combo, todavia no esta terminado

--- resources/views/tienda/index.blade.php
+++ resources/views/tienda/index.blade.php
@@ -17,7 +17,7 @@
                         <ul class="list-group list-group-flush">
                             <li class="list-group-item">Stock Disponible:</li>
                             <li class="list-group-item">
-                                <select class="form-select" aria-label="Default select example" name="talle">
+                                <select class="form-select" aria-label="Default select example" name="talle" >
                                     <option selected>Seleccione talle</option>
                                         @foreach ($talles as $talla)
                                             <option value={{ $talla->id }}>{{ $talla->talla }}</option>
@@ -27,7 +27,7 @@
                         </ul>
                         <div class="card-body">
                             <input type="hidden" value="{{ $producto->id}}" name="idProducto">
-                            <input type="hidden" value="1" name="cantidad">
+                            <input type="text" value="1" id="cantidad" name="cantidad">
                             <button type="submit" class="btn btn-success">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-patch-plus-fill" viewBox="0 0 16 16">
                                     <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zM8.5 6v1.5H10a.5.5 0 0 1 0 1H8.5V10a.5.5 0 0 1-1 0V8.5H6a.5.5 0 0 1 0-1h1.5V6a.5.5 0 0 1 1 0z"/>
@@ -45,5 +45,20 @@
     $(document).ready(function () {
         $( "#a-tienda" ).toggleClass("active");
     });
+
+    const csrfToken=document.head.querySelector("[name~=csrft-token][content]").content;
+    document.getElementById('cantidad').addEventListener('change', (e)=>{
+        fetch('cantidad',{
+            method: 'POST',
+            body: JSON-stringify({
+                id_indumentaria: e.target.value,
+                id_talla: 'ingrese valor'
+            })
+            header:{
+                'Content-Type': 'application/json',
+                "X-CSRF-Token": csrfToken
+            }
+        })
+    })
 </script>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -75,5 +75,6 @@ Route::post('/add', [CartController::class, 'add'])->name('cart.store');
 Route::post('/update', [CartController::class, 'update'])->name('cart.update');
 Route::post('/remove', [CartController::class, 'remove'])->name('cart.remove');
 Route::post('/clear', [CartController::class, 'clear'])->name('cart.clear');
+Route::get('/cantidadstock', [StockController::class, 'getCantidadStock']);
 
 // Route::post('/home', [App\Http\Controllers\HomeController::class, 'inicio']);
